128714f215935b745c47aed99552510362dd1dae
francktamala <francktamala@gmail.com>
2021-11-12 14:01:08 +0200

    Code refactored

--- App.js
+++ App.js
@@ -5,8 +5,8 @@ import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
 
-
-  async function changePicture() {
+  // A function that allow the user to access his camera
+  async function takePicture() {
     if (Platform.OS !== 'web') {
       const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
       if (status !== 'granted') {
@@ -20,43 +20,53 @@ export default function App() {
           quality: 1,
         });
 
-        if (!result.cancelled) {
-          const image = {
-            uri: result.uri,
-            type: 'image/jpeg',
-            name: 'myImage' + '-' + Date.now() + '.jpg'
-          }
-          // Instantiate a FormData() object
-          const imgBody = new FormData();
-          // append the image to the object with the title 'image'
-          imgBody.append('data', image);
-
-          console.log(imgBody)
-          const url = `https://api.everypixel.com/v1/faces`;
-          // Perform the request. Note the content type - very important
-          fetch(url, {
-            method: 'POST',
-            headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'multipart/form-data',
-              'Authorization': "Basic V0dGYmJwUkV1YmliNExtSTk2TWEzakRZOmE3a2luOXZyNG9EcWtqc2RScXBaNGdLeENZY1QzMW5CYUtGSjdoc3RUb0I3YnAzdQ=="
-            },
-            body: imgBody
-          }).then(res => res.json()).then(results => {
-            // Just me assigning the image url to be seen in the view
-            console.log(results, results.faces[0].age);
-            alert("You are " + results.faces[0].age)
-          }).catch(error => {
-            console.error(error);
-          });
-        }
+        return result;
       }
+    }
+  }
+
+  async function onUploadPictureClicked() {
+    // Calling Take Picture Function
+    const result = await takePicture();
+
+    if (!result.cancelled) {
+
+      // Creating the image
+      const image = {
+        uri: result.uri,
+        type: 'image/jpeg',
+        name: 'myImage' + '-' + Date.now() + '.jpg'
+      }
+
+      // Instantiate a FormData() object
+      const imgBody = new FormData();
+      // append the image to the object with the title 'image'
+      imgBody.append('data', image);
+
+      console.log(imgBody)
+      const url = `https://api.everypixel.com/v1/faces`;
+      // Perform the request. Note the content type - very important
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'multipart/form-data',
+          'Authorization': "Basic V0dGYmJwUkV1YmliNExtSTk2TWEzakRZOmE3a2luOXZyNG9EcWtqc2RScXBaNGdLeENZY1QzMW5CYUtGSjdoc3RUb0I3YnAzdQ=="
+        },
+        body: imgBody
+
+      }).then(res => res.json()).then(results => {
+        // Getting the data back from the API
+        alert("You are " + results.faces[0].age)
+      }).catch(error => {
+        console.error(error);
+      });
     }
   }
 
   return (
     <View style={styles.container}>
-      <Button title="Upload" onPress={changePicture} />
+      <Button title="Upload" onPress={onUploadPictureClicked} />
       <StatusBar style="auto" />
     </View>
   );
